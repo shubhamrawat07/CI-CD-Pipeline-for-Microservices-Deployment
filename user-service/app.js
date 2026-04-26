dc051cd93914e2bab298a35aa5fc196ef5058794
Shubham Rawat <rawatji2210@gmail.com>
2026-04-26 10:00:39 +0000

    6th commit

--- user-service/app.js
+++ user-service/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req,res)=>{
-    res.send("User Service Running after 5th upgrade");
+    res.send("User Service Running after 6th upgrade");
 });
 
 app.listen(3000, ()=>{
