46918ab19035b252daa32f5b3db9822cbedfc1dc
Shubham Rawat <rawatji2210@gmail.com>
2026-04-26 10:57:46 +0000

    Product commit 3

--- product-service/app.js
+++ product-service/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req,res)=>{
-    res.send("Product Service 2 Running");
+    res.send("Product Test 3 Running");
 });
 
 app.listen(3000, ()=>{
