50d7aef6268c34c840d1b3d910c7eeb379af512a
Shubham Rawat <rawatji2210@gmail.com>
2026-04-26 09:14:40 +0000

    Test Jenkins automation: change to Hello World

--- user-service/app.js
+++ user-service/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req,res)=>{
-    res.send("User Service Running after upgrade");
+    res.send("User Service Running after 2nd upgrade");
 });
 
 app.listen(3000, ()=>{
